88adc163c30377f30f6155fdb457e8d6845f6e58
Neeraj Kushwaha <neerajk.contact@gmail.com>
2023-04-19 15:07:27 +0530

    init

--- pages/work/campaigns.js
+++ pages/work/campaigns.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import Head from 'next/head'
 
 import Work from '.'
 
-export default function Branding() {
+export default function Compaigns() {
   return (
     <Work>
-        Branding
+        <Head>
+            <title>Work: Branding</title>
+        </Head>
+
+        <div className="WorkHero">
+            <div className="WorkHero-wrapper">
+                <h1 className="WorkHero-title HomeTitle">
+                    <span className="HomeTitle-title app-text--regular">BRANDING</span>
+                </h1>
+                <h2 className="WorkHero-subtitle app-text--large">
+                    <p>
+                        We help co-create brands, not only advertise your projects. Consider us the Google Maps equivalent for your brand.
+                        We’ll enable your brand to not only set the right success destinations, but also pick the most optimal routes to get to these goals.
+                    </p>
+                </h2>
+            </div>
+        </div>
     </Work>
   )
 }
-
-
